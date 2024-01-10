bb0ddb72c9835f09dddc73860e2656f8fa0c1537
BrandanGG <101829241+BrandanGG@users.noreply.github.com>
2024-01-10 07:14:58 -0500

    First Commit

---
+++ src/main/java/com/brandan/newguns/Hoe.java
@@ -0,0 +1,107 @@
+package com.brandan.newguns;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Hoe implements CommandExecutor, TabCompleter {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (sender instanceof Player) {
+            if (args.length == 1 && command.getName().equalsIgnoreCase("Hoe") || (args.length == 0 && command.getName().equalsIgnoreCase("Hoe"))) {
+                if (command.getName().equalsIgnoreCase("Hoe")) {
+                    sender.sendMessage(Component.text("Usage: /hoe {Player} {Arguments}", NamedTextColor.RED));
+                    return true;
+                }
+            }
+
+            if (args.length == 2 && command.getName().equalsIgnoreCase("Hoe")) {
+                for (Player target : Bukkit.getOnlinePlayers()) {
+                    if (target.getName().equalsIgnoreCase(args[0])) {
+                        ItemStack hoe = Hoes(args[1]);
+                        if (hoe != null) {
+                            target.getInventory().addItem(hoe);
+                            target.sendMessage(Component.text("Received a " + args[1] + " Hoe", NamedTextColor.GREEN));
+                            sender.sendMessage(Component.text(target.getName() + " received a " + args[1] + " Hoe", NamedTextColor.GREEN));
+                            return true;
+                        } else {
+                            sender.sendMessage(Component.text("Error: Invalid hoe type.", NamedTextColor.RED));
+                            return false;
+                        }
+                    }
+                }
+                sender.sendMessage(Component.text("Error: Player not found.", NamedTextColor.RED));
+            }
+        }
+        return false;
+    }
+
+    public ItemStack Hoes(String material) {
+        if (material.equalsIgnoreCase("diamond")) {
+            ItemStack diamond = new ItemStack(Material.DIAMOND_HOE);
+            ItemMeta meta = diamond.getItemMeta();
+            List<Component> lore = Collections.singletonList(Component.text("Click to launch snowballs", NamedTextColor.BLUE));
+            meta.lore(lore);
+            meta.displayName(Component.text("Snowball Launcher", NamedTextColor.WHITE));
+            diamond.setItemMeta(meta);
+            return diamond;
+        }
+        if (material.equalsIgnoreCase("iron")) {
+            ItemStack iron = new ItemStack(Material.IRON_HOE);
+            ItemMeta meta = iron.getItemMeta();
+            List<Component> lore = Collections.singletonList(Component.text("Click to launch eggs", NamedTextColor.YELLOW));
+            meta.lore(lore);
+            meta.displayName(Component.text("Egg Launcher", NamedTextColor.YELLOW));
+            iron.setItemMeta(meta);
+            return iron;
+        }
+        if (material.equalsIgnoreCase("stone")) {
+            ItemStack stone = new ItemStack(Material.STONE_HOE);
+            ItemMeta meta = stone.getItemMeta();
+            List<Component> lore = Collections.singletonList(Component.text("Click to launch tridents", NamedTextColor.BLUE));
+            meta.lore(lore);
+            meta.displayName(Component.text("Trident Launcher", NamedTextColor.WHITE));
+            stone.setItemMeta(meta);
+            return stone;
+        }
+        return null;
+    }
+
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, org.bukkit.command.@NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (command.getName().equalsIgnoreCase("Hoe")) {
+            if (args.length == 1) {
+                // If the player is typing the first argument (player name), return online player names
+                List<String> playerNames = new ArrayList<>();
+                for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                    playerNames.add(onlinePlayer.getName());
+                }
+                return playerNames;
+            } else if (args.length == 2) {
+                // If the player is typing the second argument, return valid hoe types
+                List<String> list = new ArrayList<>();
+                list.add("diamond");
+                list.add("iron");
+                list.add("stone");
+
+                return list;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/brandan/newguns/Main.java
@@ -0,0 +1,48 @@
+package com.brandan.newguns;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Egg;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Snowball;
+import org.bukkit.entity.Trident;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        Bukkit.getPluginManager().registerEvents(this, this);
+        getCommand("hoe").setExecutor(new Hoe());
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent e) {
+        Player player = e.getPlayer();
+        if (e.hasItem()) {
+            if (e.getItem().getType() == Material.DIAMOND_HOE) {
+                player.launchProjectile(Snowball.class);
+                player.sendMessage(Component.text("You have launched a snowball.", NamedTextColor.GREEN));
+            }
+            else if (e.getItem().getType() == Material.IRON_HOE) {
+                player.launchProjectile(Egg.class);
+                player.sendMessage(Component.text("You have launched an egg.", NamedTextColor.GREEN));
+            }
+            else if (e.getItem().getType() == Material.STONE_HOE) {
+                player.launchProjectile(Trident.class);
+                player.sendMessage(Component.text("You have launched a trident.", NamedTextColor.GREEN));
+            }
+        }
+    }
+
+}
